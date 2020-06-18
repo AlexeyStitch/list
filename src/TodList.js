7db7a2b9a244aa19b7f5adbadda49b6d22bfb9cf
AlexeyStitcg <ljrevtyn12!>
2020-06-18 20:32:33 +0500

    Todos

--- src/TodList.js
+++ src/TodList.js
@@ -4,7 +4,12 @@ import './App.css';
 
 function TodList(props) {
     const [editTask, setEditTask] = useState({})
-
+    const editMode = (el) => {
+        setEditTask(el)
+    }
+    const onChangeTaskEdit = (e) => {
+        setEditTask({...editTask, name: e.target.value})
+    }
 
     return (
         <div className="todo">
@@ -13,15 +18,15 @@ function TodList(props) {
                 props.todos.map(el => <li key={el.id}>
                     {el.done ? '✅' : '❌'}
                     {el.name}
-                    
+
                     <button onClick={() => props.onDoneTaskToggle(el.id)}>
                         {el.done ? 'unDone' : 'Done'}
                     </button>
 
                     {
                         editTask.id === el.id
-                        ? <input type="text" value={editTask.name} onChange={onChangeTaskEdit}/>
-                        : <button disabled></button>
+                            ? <input type="text" value={editTask.name} onChange={onChangeTaskEdit}/>
+                            : <span onClick={() => editMode(el)}>{el.name}</span>
                     }
 
                     <button onClick={() => props.onDeleteTask(el.id)}>Delete</button>
